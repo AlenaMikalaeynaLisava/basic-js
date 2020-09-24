9903ce460db7b908c53f3dc14b4b415d4f2a1e17
AlenaMikalaeynaLisava <63248586+AlenaMikalaeynaLisava@users.noreply.github.com>
2020-09-24 23:15:06 +0300

    Update dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+let result = "";
+  members.forEach((elem) => {
+    if (typeof elem === "string") {
+      let item = elem.trim();
+      if (item.length > 0) {
+        result += item[0].toUpperCase();
+      }
+    }
+  });
+  return result ? result : false;
 };
